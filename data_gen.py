7ee9b987a28182a07e8c1be39fa387eca7fc4841
ysy2000 <suebook@naver.com>
2024-01-11 02:13:30 +0900

    updates

--- data_gen.py
+++ data_gen.py
@@ -6,11 +6,13 @@ output = list of the results of baccarat game among 'player', 'banker', and 'tie
 
 import time
 import os
-from random import random 
-import matplotlib
+import random
+# import matplotlib
+import pandas as pd
 
 num_dec = 8
 num_cards_in_one_dec = 52
+cut_card = 'cutting card'
 
 num_game = 1
 
@@ -26,7 +28,10 @@ def gen_list():
             # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
             for k in range(4):
                 # spade, heart, dia, clova
-                card_name = str(j) + "_" + str(k) + "_" + str(i)
+                if j > 10:  # J, Q, K = 10
+                    card_name = "10_" + str(k) + "_" + str(i)
+                else:
+                    card_name = str(j) + "_" + str(k) + "_" + str(i)
                 # ex) 1_1_1 == 1_spade_first dec
                 cards.append(card_name)
 
@@ -38,6 +43,23 @@ def gen_list():
     return cards
     
     
+def compare(player, banker):
+    if player > banker:
+        return 'player'
+    elif banker > player:
+        return 'banker'
+    else:
+        return 'tie'
+
+def get_the_card(suffled_cards, i):
+    val = int(suffled_cards[i].split("_")[0])
+    if val == cut_card:
+        val = int(suffled_cards[i+1].split("_")[0])
+        print("The cut card has been scanned. I will change shoes after finishing this game.")
+        i += 1
+    i += 1
+
+    return val, i
 
 # play game
 def play_game(ready_cards):
@@ -47,55 +69,67 @@ def play_game(ready_cards):
 
     # value
     results = []
-    a, b, c, d = 0
 
     # draw the cards   
-    while i < total_cartds + 1:
-        try:
-            a = int(suffled_cards[i].split("_")[0])
-            b = int(suffled_cards[i+1].split("_")[0])
-            c = int(suffled_cards[i+2].split("_")[0])
-            d = int(suffled_cards[i+3].split("_")[0])
-        except ValueError:
-            print("The cut card has been scanned. I will change shoes after finishing this game.")
-            ## 컷카드 찾아서 거기만 패스 해서 값을 넣어야함.
+    i = 0
 
+    while i < total_cartds + 1:
+        a, i = get_the_card(suffled_cards, i)
+        b, i = get_the_card(suffled_cards, i)
+        c, i = get_the_card(suffled_cards, i)
+        d, i = get_the_card(suffled_cards, i)
 
         player = (a + c) % 10
         banker = (b + d) % 10
 
-        i += 4
-
         if player >= 8 or banker >= 8:
-            if player > banker:
-                results.append('player')
-            elif banker > player:
-                results.append('banker')
-            else:
-                results.append('tie')
+            results.append(compare(player, banker))
             break
 
         elif player > 5 and banker >  5:
-            if player > banker:
-                results.append('player')
-            elif banker > player:
-                results.append('banker')
-            else:
-                results.append('tie')
+            results.append(compare(player, banker))
             break
         
         elif player <= 5:
-            e = int(suffled_cards[i].split("_")[0])
-            player = (player + e) % 10
-            i += 1
+            p_extra, i = get_the_card(suffled_cards, i)
+            player = (player + p_extra) % 10
+
+            if banker <= 2:
+                b_extra, i = get_the_card(suffled_cards, i)
+                banker = (banker + b_extra) % 10
+                results.append(compare(player, banker))
+                break
             
+            elif banker == 3:
+                if p_extra == 8:
+                    b_extra, i = get_the_card(suffled_cards, i)
+                    banker = (banker + b_extra) % 10
+                    results.append(compare(player,banker))
+                    break
+                results.append(compare(player, banker))
+                break
 
+            else:
+                # 4 from 2, 5 from 4, 6 from 6 
+                if p_extra >= (2 * banker - 6) and p_extra <= 7:
+                    b_extra, i = get_the_card(suffled_cards, i)
+                    banker = (banker + b_extra) % 10
+                    results.append(compare(player,banker))
+                    break
+                results.append(compare(player, banker))
+                break
 
+        elif banker <= 5:
+            b_extra, i = get_the_card(suffled_cards, i)
+            banker = (banker + b_extra) % 10
+            results.append(compare(player,banker))
+            break
 
-
-    
-
-    return 0
+        else:
+            print("calculation error!")
+            break
+                
+    return results
 
 # burn cards procedure
 def burn_procedure(suffled_cards):
@@ -105,23 +139,24 @@ def burn_procedure(suffled_cards):
 
 # suffle
 def suffle(cards):
-    suffled_cards = suffle.cards
+    random.shuffle(cards)
 
     # insert cut card
     # we usually insert cut card before more than ones dec
-    ready_cards = suffled_cards[:num_cards_in_one_dec + round( 10 * random() )]
-    ## 이 코드가 아니라 컷카드 삽입 코드로 바꿔야함
 
-    return ready_cards
+    ## ready_cards = suffled_cards[:num_cards_in_one_dec - round( 10 * random())]
+
+    # insert cut card
+    cut_point = num_cards_in_one_dec - round( 10 * random.random() )
+    cards.insert(cut_point, cut_card)
+
+    return cards
 
 # save
-def save_results(list):
-    start = time.time()
-    time_sofar = time.time() - start
-    Taken_hour = time_sofar//3600
-    Taken_min = (time_sofar - Taken_hour*3600)//60
-    Time_info = "{}(h) {}(m) Taken".format(Taken_hour, Taken_min )
-    print(Time_info)
+def save_results(result, start):
+    file_name = str(start)
+    df = pd.DataFrame(result)
+    df.to_csv("./data/" + file_name, mode='w', index=False)
     return 0
 
 # analysis the results
@@ -130,10 +165,22 @@ def draw_plots():
 
 # main
 if __name__ == "__main__":
+    start = time.time()
+
     cards_list = gen_list()
     suffled_cards = suffle(cards_list)
+    ready_cards = burn_procedure(suffled_cards)
+    result = play_game(ready_cards)
 
+    time_sofar = time.time() - start
+    Taken_hour = time_sofar//3600
+    Taken_min = (time_sofar - Taken_hour*3600)//60
+    Time_info = "{}(h) {}(m) Taken".format(Taken_hour, Taken_min )
+    print(Time_info)
 
+    save_results(result, start)
+
+    # if you want repeat generation
     for i in range(num_game):
         play_game(suffled_cards)
 
